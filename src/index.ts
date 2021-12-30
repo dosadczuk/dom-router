9828a5a397009721dfd29bba42e646949cb1d912
dosadczuk <damian.osadczuk@gmail.com>
2021-12-30 09:29:13 +0100

    Remove router file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import { runDirective } from '@router/directives'
 import '@router/directives/index'
-import { init } from '@router/router'
+import { dispatch, Event, subscribe } from '@router/events'
 
 runDirective('data-router-cloak')
 runDirective('data-router-page')
 runDirective('data-router-link')
 
-init()
+// let app subscribe to "before init"
+dispatch(document, Event.Initialize)
+
+// update forced with link
+subscribe(document, Event.ChangePage, (event) => {
+  const { detail: route } = event as CustomEvent
+
+  history.pushState(null, '', route)
+
+  dispatch(document, Event.ChangeView)
+})
+
+// update forced with History API
+subscribe(window, 'popstate', () => {
+  dispatch(document, Event.ChangeView)
+})
+
+// force update view, let event set up app
+dispatch(document, Event.ChangeView)
+
+// let app subscribe to "after init"
+dispatch(document, Event.Initialized)
