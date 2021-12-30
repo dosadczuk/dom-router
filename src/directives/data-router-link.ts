fd9ff8aabf3487affe0f943e42adcdb1843bc7dd
dosadczuk <damian.osadczuk@gmail.com>
2021-12-30 11:06:52 +0100

    Refactor link directive

--- src/directives/data-router-link.ts
+++ src/directives/data-router-link.ts
@@ -1,20 +1,23 @@
 import { Directive, setDirective } from '@router/directives'
-import { getHTMLElementsWithDirective, HTMLElementWithDirectives } from '@router/dom'
+import { getHTMLElementsWithDirective } from '@router/dom'
 import { dispatch, Event } from '@router/events'
 
 setDirective(Directive.Link, () => {
-  getHTMLElementsWithDirective(Directive.Link).forEach(dispatchPageChangeOnClick)
-})
-
-const dispatchPageChangeOnClick = ({ content: link, directives }: HTMLElementWithDirectives): void => {
-  const route = directives.get(Directive.Link)
-  if (route == null) {
+  const links = getHTMLElementsWithDirective(Directive.Link)
+  if (links.length === 0) {
     return
   }
 
-  link.addEventListener('click', event => {
-    event.preventDefault()
+  links.forEach(({ content: link, directives }) => {
+    const route = directives.get(Directive.Link)
+    if (route == null) {
+      return
+    }
 
-    dispatch(document, Event.ChangePage, route)
+    link.addEventListener('click', event => {
+      event.preventDefault()
+
+      dispatch(document, Event.ChangePage, route)
+    })
   })
-}
+})
