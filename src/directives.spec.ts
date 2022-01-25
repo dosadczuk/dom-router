fdb12bde6949ce5d916adddfb012ba2cc4c91887
dosadczuk <damian.osadczuk@gmail.com>
2022-01-25 15:37:06 +0100

    Add test to check directive factory and cleanup

--- src/directives.spec.ts
+++ src/directives.spec.ts
@@ -4,7 +4,9 @@ import {
   getDirectives,
   getDirectivesAsSelector,
   isDirective,
+  setUpDirectives,
 } from '@router/directives'
+import type { DirectiveCleanup, DirectiveFactory } from '@router/types'
 import { afterEach, describe, expect, fn, it } from 'vitest'
 
 describe('directives', () => {
@@ -13,7 +15,7 @@ describe('directives', () => {
     clearDirectives()
   })
 
-  it('should add directive', () => {
+  it('should define directive', () => {
     // given
     const directiveName = 'test'
     const directiveFactory = fn()
@@ -28,8 +30,8 @@ describe('directives', () => {
 
   it('should list directives', () => {
     // given
-    defineDirective('test1', { factory: fn() })
-    defineDirective('test2', { factory: fn() })
+    defineDirective('test1', { factory: null })
+    defineDirective('test2', { factory: null })
 
     // when
     const directives = getDirectives()
@@ -38,10 +40,28 @@ describe('directives', () => {
     expect(directives).length(2)
   })
 
+  it('should set up directives with factory function', () => {
+    // given
+    const directiveName = 'test1'
+    const directiveCleanup: DirectiveCleanup = fn()
+    const directiveFactory: DirectiveFactory = fn(() => directiveCleanup)
+
+    defineDirective(directiveName, {
+      factory: directiveFactory,
+    })
+
+    // when
+    setUpDirectives([], [ directiveName ])
+
+    // then
+    expect(directiveFactory).toBeCalledTimes(1)
+    expect(directiveCleanup).toBeCalledTimes(1)
+  })
+
   it('should get selector to find elements with any directive', function () {
     // given
-    defineDirective('test1', { factory: fn() })
-    defineDirective('test2', { factory: fn() })
+    defineDirective('test1', { factory: null })
+    defineDirective('test2', { factory: null })
 
     // when
     const selector = getDirectivesAsSelector()
@@ -52,8 +72,8 @@ describe('directives', () => {
 
   it('should clear directives', () => {
     // given
-    defineDirective('test1', { factory: fn() })
-    defineDirective('test2', { factory: fn() })
+    defineDirective('test1', { factory: null })
+    defineDirective('test2', { factory: null })
 
     // when
     const directivesBeforeClear = getDirectives()
